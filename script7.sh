87a87bed519fe3c2931c70753af4b7e76aa7396b
sahil <sahilsharmag007@gmail.com>
2025-10-11 20:25:05 +0530

    add script7.sh

---
+++ script7.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+for i in 1 2 3 4 5
+do
+	echo "the number is $i"
+done
+echo "new loop"
+
+for i in Ram Sham Dham
+do 
+	echo "$i"
+done
+
+echo "third loop"
+
+for i in {1..4}
+do 
+	echo "the $i"
+done
+
+
+
+
+
+
+
+
+
