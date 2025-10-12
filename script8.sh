6183fc0b5b3284ab6294ba55ac815091f3aed475
sahil <sahilsharmag007@gmail.com>
2025-10-12 21:26:59 +0530

    add script8.sh

---
+++ script8.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+a="Hello World"
+#length= ${#a}
+#echo "The length is $length"
+#upper= "${a^^}"
+#echo "upper is $upper"
+lower=${a,,}
+replace="${a/World/Buddy}"
+echo $replace
+echo $lower
+slice=${a:2:4}
+echo $slice
+
+
+
+
