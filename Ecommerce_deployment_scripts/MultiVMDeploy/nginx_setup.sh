f5c230d345a09f40db48c189abe6251b88096019
sahil <sahilsharmag007@gmail.com>
2026-04-27 22:10:26 +0300

    adding seperate deployment folder

---
+++ Ecommerce_deployment_scripts/MultiVMDeploy/nginx_setup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# --- Tier 3: Nginx Infrastructure Script ---
+if ! nginx -v 2>&1 /dev/null
+then    
+    apt update && apt install nginx -y
+fi
+# 1. Define where the actual code is currently located
+PROJECT_ROOT="/home/ecom-service/ecommerce-app" 
+CONF_NAME="ecommerce"
+
+echo "Setting up Nginx for project at: $PROJECT_ROOT"
+
+# 2. Create the configuration file dynamically
+cat <<EOF > /etc/nginx/sites-available/ecommerce
+server {
+    listen 80;
+    server_name localhost;
+
+    location / {
+        proxy_pass http://127.0.0.1:8000;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+    }
+
+    location /static/ {
+        # Using the variable to ensure Nginx finds your assets
+        alias /home/ecom-service/ecommerce-app/app/static/;
+    }
+}
+EOF
+
+# 3. Move to Nginx directories with sudo
+echo "Applying system configurations..."
+sudo ln -sf /etc/nginx/sites-available/$CONF_NAME /etc/nginx/sites-enabled/
+sudo rm -f /etc/nginx/sites-enabled/default
+
+# 4. Final Safety Check & Reload
+if sudo nginx -t; then
+    sudo systemctl reload nginx
+    echo "Nginx is now proxying to port 8000."
+else
+    echo "Error: Nginx configuration is invalid."
+    exit 1
+fi
