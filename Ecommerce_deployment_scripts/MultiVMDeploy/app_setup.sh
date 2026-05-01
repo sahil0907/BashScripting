89a9bdbafadfb61f45c952776892a9d74bacd945
sahil <sahilsharmag007@gmail.com>
2026-05-01 22:51:53 +0300

    update app_setup.sh

--- Ecommerce_deployment_scripts/MultiVMDeploy/app_setup.sh
+++ Ecommerce_deployment_scripts/MultiVMDeploy/app_setup.sh
@@ -2,18 +2,20 @@
 readonly DB_NAME="ecom_db"
 readonly DB_USER="ecom_user"
 readonly DB_PASS="ecom_password"
+readonly DB_IP="192.168.15.136"
 readonly APP_USER="ecom-service"
 readonly PROJECT_DIR="/home/$APP_USER/ecommerce_app"
 readonly GIT_URL="https://github.com/sahil0907/Ecommerce-app"
 readonly SECRET_DIR="/home/$APP_USER/secrets.env"
+
 set -euo pipefail
-trap 'echo "❌ Error in function ${FUNCNAME:-main} on line $LINENO"; exit 1' ERR
+trap 'echo " Error in function ${FUNCNAME:-main} on line $LINENO"; exit 1' ERR
 
 # setting dependencies
 setup_dependencies()
 {
     echo "Adding python3-venv"
-    apt update -qq && apt install python3-venv git postgresql nginx curl  -y
+    apt update -qq && apt install python3-venv git postgresql nginx curl sshpass  -y
 
     echo "Creating service user"
     if  ! id "$APP_USER" &>/dev/null 
@@ -22,12 +24,7 @@ setup_dependencies()
     fi
  
 }
- 
-#run the database script here
-db_setup()
-{
-     
-}
+
 
 get_code()
 {
@@ -41,51 +38,71 @@ get_code()
 }
 #setup ssh communication btw app. vm and nginx vm
 setup_sshkeys()
-{
-    local USER="nginx_user"
-    local IP="192.168.15.137"
-    local PASS="4323"
+{    echo "------------------------------------------------------"
 
-    if [ -f "/home/$APP_USER/.ssh/id_rsa" ]
-then    
-    echo "Creating keys...."
-    sudo -u "$APP_USER" mkdir -p "/home/$USER/.ssh"
-    sudo -u "$APP_USER" ssh-keygen -t rsa -b 4096 -f "/home/$APP_USER/.ssh/id_rsa" -N ""
-fi
-sudo -u "$USER" sshpass -p "$PASS" ssh-copy-id -o StrictHostKeyChecking=no "$USER@$IP"
+    echo "SSH SETUP"
+    local USER="nginx_user"
+    local IP="192.168.15.136"
+    local PASS="4323"
+    #creating keys if not present
+    if [ ! -f "/home/$APP_USER/.ssh/id_rsa" ]; then
+        sudo -u "$APP_USER" mkdir -p "/home/$APP_USER/.ssh"
+        sudo -u "$APP_USER" chmod 700 "/home/$APP_USER/.ssh"
+        sudo -u "$APP_USER" ssh-keygen -t rsa -b 4096 -f "/home/$APP_USER/.ssh/id_rsa" -N ""
+    fi
+
+    sudo -u "$APP_USER" sshpass -p "$PASS" ssh-copy-id -o StrictHostKeyChecking=no "$USER@$IP"
+    echo "------------------------------------------------------"
 
 }
 
 nginx_setup()
-{
-    rsync -avz "$PROJECT_DIR/app/static/" 192.168.15.137:/var/www/static
+
+{      
+     echo "------------------------------------------------------"
+
+    echo "NGINX SETUP"
+    echo " Syncing static assets to Nginx VM..."
+    local USER="nginx_user"
+    local IP="192.168.15.136"
+
+    sudo -u "$APP_USER" rsync -avz "$PROJECT_DIR/app/static/" "$USER@$IP":/var/www/static/
+    echo "------------------------------------------------------"
 }
 
 
 python_dependencies()
-{
+{    echo "------------------------------------------------------"
+
+    echo "checking dependencesi"
     sudo -u "$APP_USER" -H  python3 -m venv "$PROJECT_DIR/venv"
     sudo -u "$APP_USER" -H "$PROJECT_DIR/venv/bin/pip" install --upgrade pip
     sudo -u "$APP_USER" -H "$PROJECT_DIR/venv/bin/pip" install --upgrade flask flask-sqlalchemy python-dotenv psycopg2-binary gunicorn
     #manual schema trigger
     sudo -u "$APP_USER" -H bash -c "export \$(cat $SECRET_DIR | xargs) && cd $PROJECT_DIR/app && ../venv/bin/python3 -c 'from app import app, init_db; init_db()'"
+    echo "------------------------------------------------------"
 }
 
 provision_secrets()
-{
+{    echo "------------------------------------------------------"
+
+    echo "Secret provisioning"
     local secretkey=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 32)
 
     cat <<EOF > "$SECRET_DIR"
-DATABASE_URL=postgresql://$DB_USER:$DB_PASS@192.168.15.137/$DB_NAME
+DATABASE_URL=postgresql://$DB_USER:$DB_PASS@$DB_IP/$DB_NAME
 SECRET_KEY=$secretkey
 EOF 
     chmod 600 "$SECRET_DIR"
     chown "$APP_USER":"$APP_USER" "$SECRET_DIR"
+    echo "------------------------------------------------------"
 }
 
 creating_service()
 {
-    cat <<EOF > /etc/systemd/system/ecommerce.service
+    echo "------------------------------------------------------"
+    echo "Creating service user"
+cat <<EOF > /etc/systemd/system/ecommerce.service
 [Unit]
 Description=Gunicorn Ecommerce Service
 
@@ -100,6 +117,8 @@ Restart=always
 [Install]
 WantedBy=multi-user.target
 EOF
+echo "-------------------------------------"
+echo "restarting the service"
         systemctl daemon-reload
         systemctl enable ecommerce
         systemctl restart ecommerce
@@ -108,29 +127,17 @@ EOF
 main()
 {
     setup_dependencies
-    provision_secrets
     get_code
+    provision_secrets
+    setup_sshkeys
     nginx_setup
-    db_setup
     python_dependencies
     creating_service
 
 
 }
 
-
 main
-
-
-
-
-
-
-
-
-
-
-
-
+ 
 
 
